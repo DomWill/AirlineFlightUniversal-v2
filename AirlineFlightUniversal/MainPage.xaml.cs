3aa136e7f16b8f0d3a6149b04b7428163fc50c48
Dominic Williamson <Dominic_W@live.com.au>
2017-10-07 11:56:26 +1000

    3 Column UI base complete

--- AirlineFlightUniversal/MainPage.xaml.cs
+++ AirlineFlightUniversal/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using AirlineFlightUniversal.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,7 @@ namespace AirlineFlightUniversal
         public MainPage()
         {
             this.InitializeComponent();
+            ContactsCVS.Source = Contact.GetContactsGrouped(250);
         }
     }
 }
---
+++ AirlineFlightUniversal/Model/GroupInfoList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AirlineFlightUniversal.Model
+{
+    public class GroupInfoList : List<object>
+    {
+        public object Key { get; set; }
+    }
+}
